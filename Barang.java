26e6665bda9b166661b57e0f63a9bc48f24c15a3
enopinka <retnopinka20@gmail.com>
2023-09-23 09:18:47 +0700

    menambahkan komentar

--- Barang.java
+++ Barang.java
@@ -1,18 +1,24 @@
+// Nama : Retno Pinka Pratiwi
+// NIM  : L012237
+
+//kelas barang
 public class Barang {
+  // atribut-atribut
   private String nama;
   private int harga;
   private int jumlah;
 
+  // constructor
   public Barang(
       String nama,
       int harga,
-      int jumlah
-  ) {
+      int jumlah) {
     this.nama = nama;
     this.harga = harga;
     this.jumlah = jumlah;
   }
 
+  // method-method getter dan setter untuk mengambil nama, jumlah, dan harga
   public String getNama() {
     return nama;
   }
--- PPBO_02_L0122137.java
+++ PPBO_02_L0122137.java
@@ -1,31 +1,41 @@
 // Nama : Retno Pinka Pratiwi
 // NIM  : L0122137
 
+//import packages yang digunakan 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PPBO_02_L0122137 {
+  // atribut-atribut berisi variabel yang diminta pada soal
   public static final int PPN = 11;
   public static int totalTransaksi;
-  private ArrayList<Barang> arrayListBarang = new ArrayList<Barang>();
   private int kasToko = 0;
+  // arraylist untuk menambahkan objek barang ke dlaam list
+  private ArrayList<Barang> arrayListBarang = new ArrayList<Barang>();
 
   public static void main(String[] args) {
+    // membuat objek baru dengan nama swalayan
     PPBO_02_L0122137 swalayan = new PPBO_02_L0122137();
+
+    // membuka scanner
     Scanner scanner = new Scanner(System.in);
 
-    swalayan.arrayListBarang.add(new Barang("Bear Brand", 15000, 5));
-    swalayan.arrayListBarang.add(new Barang("Cimory Fresh Milk", 10000, 5));
-    swalayan.arrayListBarang.add(new Barang("Frisian Flag", 15000, 5));
-    swalayan.arrayListBarang.add(new Barang("Greenfields", 15000, 5));
-    swalayan.arrayListBarang.add(new Barang("Ultra Milk", 12000, 5));
+    // menambahkan barang-barang setelah program dijalankan
+    swalayan.arrayListBarang.add(new Barang("Bear Brand", 11000, 24));
+    swalayan.arrayListBarang.add(new Barang("Cimory Fresh Milk", 7500, 24));
+    swalayan.arrayListBarang.add(new Barang("Frisian Flag", 7000, 24));
+    swalayan.arrayListBarang.add(new Barang("Greenfields", 8500, 24));
+    swalayan.arrayListBarang.add(new Barang("Ultra Milk", 6500, 24));
 
     while (true) {
+      // mencetak menu layanan swalayan
       int opsi = swalayan.cetakMenu(scanner);
 
       if (opsi == 1) {
+        // penjualan barang
         System.out.println("Penjualan barang");
 
+        // memasukkan nama dan jumlah barang yang akan dijual
         System.out.print("Nama barang   : ");
         String nama = scanner.nextLine();
 
@@ -33,65 +43,79 @@ public class PPBO_02_L0122137 {
         int jumlah = scanner.nextInt();
         scanner.nextLine();
 
+        // memanggil method jualBarang() untuk melanjutkan transaksi
         swalayan.jualBarang(nama, jumlah, scanner);
+
       } else if (opsi == 2) {
+        // restock barang jika ada barang baru yang masuk ke dalam gudang
         System.out.println("Restock barang");
 
+        // memasukkan nama dan jumlah barang yang bertambah jumlahnya
         System.out.println("Nama barang : ");
         String nama = scanner.nextLine();
 
         System.out.print("Jumlah barang : ");
         int jumlah = scanner.nextInt();
 
+        // memanggil method restockBarang() untuk menambahkan jumlah barang
         swalayan.restokBarang(nama, jumlah);
+
       } else if (opsi == 3) {
-        // tambah barang baru
-        System.out.println("Nama barang baru: ");
-        System.out.print(">> ");
+        // tambah barang baru yang belum ada di gudang
+        // memasukkan nama, harga, dan jumlah barang yang ingin ditambahkan
+        System.out.print("Nama barang baru: ");
         String nama = scanner.nextLine();
 
-        System.out.println("Harga barang baru: ");
-        System.out.print(">> ");
+        System.out.print("Harga barang baru: ");
         int harga = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("Jumlah barang baru: ");
-        System.out.print(">> ");
+        System.out.print("Jumlah barang baru: ");
         int jumlah = scanner.nextInt();
         scanner.nextLine();
 
+        // memanggil method tambahBarangBaru() untuk membuat objek baru
         swalayan.tambahBarangBaru(nama, harga, jumlah);
       } else if (opsi == 4) {
-        // status toko
-        swalayan.tampilkanStatusToko();
+        // laporan penjualan dan transaksi toko
+        swalayan.cetakLaporanToko();
+
       } else {
+        // opsi selain 1-4 keluar program
         break;
       }
 
     }
 
-    swalayan.restokBarang("barang2", 3);
+    // mencetak daftar barang di dalam gudang
     swalayan.cetakDaftarBarang();
+
+    // menutup scanner
     scanner.close();
   }
 
-  private void tampilkanStatusToko() {
+  // method untuk mencetak laporan toko
+  private void cetakLaporanToko() {
     System.out.println("Laporan Toko");
 
+    // mencetak jumlah transaksi yang dilakukan dan total uang kas yang masuk
     System.out.printf("Total transaksi : %d\n", PPBO_02_L0122137.totalTransaksi);
     System.out.printf("Total uang kas  : Rp %d\n", this.kasToko);
   }
 
+  // mencetak menu layanan seperti jual, restock, dll yang
+  // mengembalikan integer berupa opsi yang dipilih user
   private int cetakMenu(Scanner scanner) {
     System.out.println("Minimarket Sederhana");
 
+    // memanggil method cetakDaftarBarang untuk mencetak barang yang tersedia
     cetakDaftarBarang();
 
     System.out.println("Opsi:");
     System.out.println("1. Jual barang");
     System.out.println("2. Restok barang");
     System.out.println("3. Tambah barang baru");
-    System.out.println("4. Status toko");
+    System.out.println("4. Laporan toko");
     System.out.println("5. Keluar");
     System.out.print(">> ");
 
@@ -101,6 +125,7 @@ public class PPBO_02_L0122137 {
     return opsi;
   }
 
+  // mencetak daftar barang di dalam gudang yang disajikan dalam bentuk tabel
   private void cetakDaftarBarang() {
     System.out.println("----------------------------------------------------------");
     System.out.printf("| %-24s | %-16s | %-8s |\n", "Nama barang", "Harga satuan", "Jumlah");
@@ -117,28 +142,38 @@ public class PPBO_02_L0122137 {
     System.out.println("----------------------------------------------------------");
   }
 
+  // method jualBarang() ketika ada pembeli
   private void jualBarang(String nama, int jumlah, Scanner scanner) {
+    // inisialisasi index dengan -1 yang artinya barang yang dicari belum ada
     int index = -1;
+    // mencari barang dengan nama yang sama seperti yang diketikkan pada input
     for (int i = 0; i < arrayListBarang.size(); i++) {
       if (arrayListBarang.get(i).getNama().equalsIgnoreCase(nama)) {
+        // ketika barang ditemukan, nilai index berubah menjadi index barang yang dicari
         index = i;
         break;
       }
     }
 
     if (index == -1) {
-      System.out.println("Barang tidak ada");
+      // ketika barang yang dicari tidak ditemukan
+      System.out.println("Barang tidak ditemukan!");
     } else {
+      // ketika barang yang dicari sudah ditemukan, maka ambil objek tersebut dari
+      // arrayList
       Barang barangKeIndex = arrayListBarang.get(index);
 
-      // validasi jumlah barang
+      // validasi jumlah barang di gudang dengan jumlah barang yang diminta
       if (barangKeIndex.getJumlah() >= jumlah) {
+        // barang di dalam gudang memenuhi jumlah yang diminta
         arrayListBarang.get(index).setJumlah(barangKeIndex.getJumlah() - jumlah);
 
+        // aritmatika untuk transaksi penjualan
         int totalHarga = arrayListBarang.get(index).getHarga() * jumlah;
         double totalPPN = totalHarga * ((double) PPBO_02_L0122137.PPN / 100);
         int totalBayar = totalHarga + (int) totalPPN;
 
+        // cetak hasil transaksi penjualan
         System.out.printf("Total harga      : Rp%16d\n", totalHarga);
         System.out.printf("PPN              : Rp%16d\n", (int) totalPPN);
         System.out.printf("Total pembayaran : Rp%16d\n", (int) totalBayar);
@@ -146,67 +181,89 @@ public class PPBO_02_L0122137 {
         boolean transaksiBerhasil = false;
 
         while (!transaksiBerhasil) {
+          // memasukkan uang untuk membayar
           System.out.println("Masukkan uang Anda");
           System.out.print(">> ");
           int bayar = scanner.nextInt();
           scanner.nextLine();
 
           if (bayar >= totalBayar) {
+            // ketika uang yang dibayarkan cukup
             kasToko += totalBayar;
             PPBO_02_L0122137.totalTransaksi++;
 
             int kembalian = bayar - totalBayar;
             System.out.printf("Kembalian        : Rp%16d\n", kembalian);
 
-            System.out.println("Transaksi sukses");
+            System.out.println("Transaksi sukses!");
             transaksiBerhasil = true;
           } else {
-            System.out.println("duit ga cukup");
+            // ketika uang tidak memenuhi, menampilkan pesan eror dan program meminta
+            // jumlah uang lagi
+            System.out.println("Uang tidak memenuhi");
           }
         }
       } else {
-        System.out.println("Barang tidak cukup");
+        // ketika barang di dalam gudang tidak memenuhi jumlah yang diminta
+        System.out.println("Barang tidak mencukupi");
       }
     }
+    System.out.println("");
   }
 
+  // method restokBarang() untuk menambahkan barang dalam gudang
   private void restokBarang(String nama, int jumlah) {
+    // inisialisasi index dengan -1 yang artinya barang yang dicari belum ada
     int index = -1;
+    // mencari barang dengan nama yang sama seperti yang diketikkan pada input
     for (int i = 0; i < arrayListBarang.size(); i++) {
       if (arrayListBarang.get(i).getNama().equalsIgnoreCase(nama)) {
+        // ketika barang ditemukan, nilai index berubah menjadi index barang yang dicari
         index = i;
         break;
       }
     }
 
     if (index != -1) {
+      // ketika barang yang dicari sudah ditemukan, maka ambil objek tersebut dari
+      // arrayList
       Barang barangKeIndex = arrayListBarang.get(index);
 
+      // mengubah jumlah barang dalam gudang menggunakan setter
       arrayListBarang.get(index).setJumlah(barangKeIndex.getJumlah() + jumlah);
 
     } else {
-      System.out.println("tdk ada");
+      // ketika barang yang dicari tidak ditemukan
+      System.out.println("Nama barang tidak ditemukan!");
+      System.out.println("Cek kesalahan tulis atau tambahkan barang baru");
     }
 
+    System.out.println("");
   }
 
+  // method untuk menambahkan barang baru yang belum pernah ada di gudang
   private void tambahBarangBaru(String nama, int harga, int jumlah) {
     // cek apakah barang sudah ada
+    // inisialisasi index dengan -1 yang artinya barang yang dicari belum ada
     int index = -1;
-    for (int a = 0; a < this.arrayListBarang.size(); a++) {
-      if (this.arrayListBarang.get(a).getNama().equalsIgnoreCase(nama)) {
-        index = a;
+    // mencari barang dengan nama yang sama seperti yang diketikkan pada input
+    for (int i = 0; i < this.arrayListBarang.size(); i++) {
+      if (this.arrayListBarang.get(i).getNama().equalsIgnoreCase(nama)) {
+        // ketika barang ditemukan, nilai index berubah menjadi index barang yang dicari
+        index = i;
         break;
       }
     }
 
     if (index != -1) {
-      // barang sudah ada
+      // barang yang ingin ditambahkan ternyata sudah ada di dalam gudang
       System.out.println("barang sudah ada");
     } else {
+      // membuat barang(objek) baru
       System.out.println("Menambahkan barang baru...");
       Barang barangBaru = new Barang(nama, harga, jumlah);
       this.arrayListBarang.add(barangBaru);
     }
+    System.out.println("");
   }
 }
